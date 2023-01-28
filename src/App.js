d4860007a778de4aded23d2408cfaee4b3dd411d
Sagar Bisht <itssagar11@gmail.com>
2023-01-28 19:07:49 +0530

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./css/App.css";
 import MovieCard from "./movieCard";
 //76703b18
 
-const movie={
-    "Title": "Italian Spiderman",
-    "Year": "2007",
-    "imdbID": "tt2705436",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
-}
-const ApiFire= async(search)=>{
-    const response= await fetch(`http://www.omdbapi.com/?apikey=76703b18&s=${search}`);
-    const data=await response.json()
-    console.log(data.Search);
-}
+
+
 const App= ()=>{
-    ApiFire("spiderman")
+    const [movies,setMovies]= useState([]);
+    const [search,setSearch]=useState("");
+    const ApiFire= async(search)=>{
+        const response= await fetch(`http://www.omdbapi.com/?apikey=76703b18&s=${search}`);
+        const data=await response.json()
+         console.log(data.Search);
+         setMovies(data.Search);
+        
+    }
+
+    useEffect(()=>{
+        ApiFire("thor");
+    },[])
+ 
     return (
         <div className="container">
-            <input
-                placeholder="Search For Movie"
+            <label>
+  <input type="text" required 
+  value={search}
+    onChange={(e)=>{
+       setSearch(e.target.value);
+      
 
-            
-            />
-              <div className="movie-card">
-                <MovieCard movie={movie} />
+    }}
+  
+  />
+  <ul>
+    <li s>s</li>
+    <li e>e</li>
+    <li a>a</li>
+    <li r>r</li>
+    <li c>c</li>
+    <li h>h</li>
+    
+  </ul>
+  
+</label>
+<button className="button"
+    onClick={()=>{
+        ApiFire(search)
+    }
+       
+    }
+
+>Search </button>
+              <div className="movie-cards">
+              <div className="hero-container">
+                {   movies!=undefined  ?
+                        movies.map((movie)=>{
+                                // console.log(movie);
+                                <MovieCard movie={movie} />
+                            })
+
+                :console.log("sd")
+                    // movies.length>0?
+                    // (movies.map((movie)=>{
+                    //     // console.log(movie);
+                    //     <MovieCard movie={movie} />
+                    // })):console.log("E")
+                 }
+                
+               
+                
+                </div>
              </div>
         </div>
     )
--- src/movieCard.jsx
+++ src/movieCard.jsx
@@ -3,13 +3,28 @@ import "./css/movieCard.css"
 const MovieCard=({  movie })=>{
     return (
       
-              <div className="card">
-                 <img src={movie.Poster} alt="Avatar" />
-                <div className="container">
-                    <h4><b>{movie.Title}</b></h4>
-                    <p>{movie.Type}</p>
-                </div>
-                </div>
+		<div className="main-container">
+			<div className="poster-container">
+				<a href="#"><img src={movie.Poster} className="poster" /></a>
+			</div>
+			<div className="ticket-container">
+				<div className="ticket__content">
+					<h2 className="ticket__movie-title">{movie.Title}</h2>
+					<p className="ticket__movie-slogan">
+                    {movie.Type}
+					</p>
+					
+					<p className="ticket__old-price">{movie.Year}</p>
+					<button className="ticket__buy-btn">Buy now</button>
+				</div>
+			</div>
+		</div>
+
+        
+        
+
+		
+
           
     )
 }
